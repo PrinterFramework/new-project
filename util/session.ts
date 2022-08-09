70a25ded8c39bdb89bed7bb5d7a59e189cb274d0
Chris Cates <hello@chriscates.ca>
2022-08-09 11:02:55 -0400

    v1.2.1

--- util/session.ts
+++ util/session.ts
@@ -1,4 +1,4 @@
-export const password = '2084fb109ba9025e899a7bedb4f39be8bfb8ac4b9048c53d5264379ba6a63038';
+export const password = '688e0189aa0927909e6ad29fe41835982fbb2a896cb4600e4bc795a3c8a68258';
 
 export const Session = {
   cookieName: 'printer',
