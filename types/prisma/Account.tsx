d5e9db8ebaedd81dd7bbd52a05384219b9a1ba97
Chris Cates <hello@chriscates.ca>
2023-10-08 17:00:21 -0400

    v2.2.2

--- types/prisma/Account.tsx
+++ types/prisma/Account.tsx
@@ -1,11 +1,11 @@
 import PostType from 'types/prisma/Post'
 
 export interface AccountType {
-	id?: string
-	username?: string
-	posts?: PostType[]
-	dateCreated?: Date
-	dateUpdated?: Date
+  id?: string
+  username?: string
+  posts?: PostType[]
+  dateCreated?: Date
+  dateUpdated?: Date
 }
 
 export default AccountType
--- types/prisma/Post.tsx
+++ types/prisma/Post.tsx
@@ -1,13 +1,13 @@
 import AccountType from 'types/prisma/Account'
 
 export interface PostType {
-	id?: string
-	accountId?: string
-	title?: string
-	content?: string
-	account?: AccountType
-	dateCreated?: Date
-	dateUpdated?: Date
+  id?: string
+  accountId?: string
+  title?: string
+  content?: string
+  account?: AccountType
+  dateCreated?: Date
+  dateUpdated?: Date
 }
 
 export default PostType
